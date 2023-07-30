ece10e25d6072d32fff0bdf1283d41c66c86b46a
BranLWill <bwillett@knights.ucf.edu>
2023-07-30 11:10:06 -0400

    finalized project

--- backend/README.md
+++ backend/README.md
@@ -102,3 +102,311 @@ createdb trivia_test
 psql trivia_test < trivia.psql
 python test_flaskr.py
 ```
+
+## API Reference
+
+### Getting Started 
+Base URL: Currently this application is only hosted locally. The backend is hosted at http://127.0.0.1:5000/
+Authentication: This version does not require authentication or API keys.
+
+### Error Handling
+
+There are four types of errors the API will return`;
+- 400 - bad request
+- 404 - resource not found
+- 422 - unprocessable
+
+### Endpoints
+
+#### GET '/categories'
+- Fetches a dictionary of all available categories.
+- Returns an object with a single key, categories, that contains a object of id: category_string key:value pairs. 
+- Sample: `curl http://127.0.0.1:5000/categories`
+```
+{
+  "categories: {
+    '1' : "Science",
+    '2' : "Art",
+    '3' : "Geography",
+    '4' : "History",
+    '5' : "Entertainment",
+    '6' : "Sports"
+    },
+  "success": true
+}
+```
+
+#### GET '/categories/<int:id>/questions'
+- Gets all questions in a specified category by id using url parameters
+- Returns a JSON object with paginated questions from a specified category
+- Sample: `curl http://127.0.0.1:5000/categories/3/questions`
+```
+{
+  "current_category": "Geography",
+  "questions": [
+    {
+      "answer": "Lake Victoria",
+      "category": 3,
+      "difficulty": 2,
+      "id": 13,
+      "question": "What is the largest lake in Africa?"
+    },
+    {
+      "answer": "The Palace of Versailles",
+      "category": 3,
+      "difficulty": 3,
+      "id": 14,
+      "question": "In which royal palace would you find the Hall of Mirrors?"
+    },
+    {
+      "answer": "Agra",
+      "category": 3,
+      "difficulty": 2,
+      "id": 15,
+      "question": "The Taj Mahal is located in which Indian city?"
+    }
+  ],
+  "success": true,
+  "total_questions": 20
+}
+```
+
+#### GET '/questions'
+- Returns a list of questions
+  - Includes a list of categories
+  - Paginated in groups of 10
+  - Includes details of question such as category, difficulty, answer and id
+- Sample: `curl http://127.0.0.1:5000/questions`
+```
+{
+  "categories": {
+    "1": "Science",
+    "2": "Art",
+    "3": "Geography",
+    "4": "History",
+    "5": "Entertainment",
+    "6": "Sports"
+  },
+  "questions": [
+    {
+      "answer": "Maya Angelou",
+      "category": 4,
+      "difficulty": 2,
+      "id": 5,
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    },
+    {
+      "answer": "Muhammad Ali",
+      "category": 4,
+      "difficulty": 1,
+      "id": 9,
+      "question": "What boxer's original name is Cassius Clay?"
+    },
+    {
+      "answer": "Apollo 13",
+      "category": 5,
+      "difficulty": 4,
+      "id": 2,
+      "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+    },
+    {
+      "answer": "Tom Cruise",
+      "category": 5,
+      "difficulty": 4,
+      "id": 4,
+      "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+    },
+    {
+      "answer": "Edward Scissorhands",
+      "category": 5,
+      "difficulty": 3,
+      "id": 6,
+      "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?"
+    },
+    {
+      "answer": "Brazil",
+      "category": 6,
+      "difficulty": 3,
+      "id": 10,
+      "question": "Which is the only team to play in every soccer World Cup tournament?"
+    },
+    {
+      "answer": "Uruguay",
+      "category": 6,
+      "difficulty": 4,
+      "id": 11,
+      "question": "Which country won the first ever soccer World Cup in 1930?"
+    },
+    {
+      "answer": "George Washington Carver",
+      "category": 4,
+      "difficulty": 2,
+      "id": 12,
+      "question": "Who invented Peanut Butter?"
+    },
+    {
+      "answer": "Lake Victoria",
+      "category": 3,
+      "difficulty": 2,
+      "id": 13,
+      "question": "What is the largest lake in Africa?"
+    },
+    {
+      "answer": "The Palace of Versailles",
+      "category": 3,
+      "difficulty": 3,
+      "id": 14,
+      "question": "In which royal palace would you find the Hall of Mirrors?"
+    }
+  ],
+  "success": true,
+  "total_questions": 20
+}
+```
+
+#### POST '/questions'
+- Creates a new question using JSON request parameters in the database
+- Sample: `curl http://127.0.0.1:5000/questions -X POST -H "Content-Type: application/json" -d '{"question": "which is the champion team of the champions 2020?", "answer": "Bayern", "difficulty": 3, "category": "6" }'`
+- Created question:
+```
+{
+    "id": 25,
+    "question": "which is the champion team of the champions 2020?",
+    "answer": "Bayern", 
+    "difficulty": 3,
+    "category": 6
+}
+```
+- JSON response:
+```
+{
+  "created": 25,
+  "question_created": "which is the champion team of the champions 2020?",
+  "questions": [
+    {
+      "answer": "Apollo 13",
+      "category": 5,
+      "difficulty": 4,
+      "id": 2,
+      "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+    },
+    {
+      "answer": "Tom Cruise",
+      "category": 5,
+      "difficulty": 4,
+      "id": 4,
+      "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+    },
+    {
+      "answer": "Maya Angelou",
+      "category": 4,
+      "difficulty": 2,
+      "id": 5,
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    },
+    {
+      "answer": "Edward Scissorhands",
+      "category": 5,
+      "difficulty": 3,
+      "id": 6,
+      "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?"
+    },
+    {
+      "answer": "Muhammad Ali",
+      "category": 4,
+      "difficulty": 1,
+      "id": 9,
+      "question": "What boxer's original name is Cassius Clay?"
+    },
+    {
+      "answer": "Brazil",
+      "category": 6,
+      "difficulty": 3,
+      "id": 10,
+      "question": "Which is the only team to play in every soccer World Cup tournament?"
+    },
+    {
+      "answer": "Uruguay",
+      "category": 6,
+      "difficulty": 4,
+      "id": 11,
+      "question": "Which country won the first ever soccer World Cup in 1930?"
+    },
+    {
+      "answer": "George Washington Carver",
+      "category": 4,
+      "difficulty": 2,
+      "id": 12,
+      "question": "Who invented Peanut Butter?"
+    },
+    {
+      "answer": "Lake Victoria",
+      "category": 3,
+      "difficulty": 2,
+      "id": 13,
+      "question": "What is the largest lake in Africa?"
+    },
+    {
+      "answer": "The Palace of Versailles",
+      "category": 3,
+      "difficulty": 3,
+      "id": 14,
+      "question": "In which royal palace would you find the Hall of Mirrors?"
+    }
+  ],
+  "success": true,
+  "total_questions": 21
+}
+```
+
+#### POST '/questions'
+- Searches for questions using a search term, 
+- Returns a JSON object with paginated questions matching the search term
+- Sample: `curl http://127.0.0.1:5000/questions -X POST -H "Content-Type: application/json" -d '{"searchTerm": "author"}'`
+```
+{
+  "questions": [
+    {
+      "answer": "Tom Cruise",
+      "category": 5,
+      "difficulty": 4,
+      "id": 4,
+      "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+    }
+  ],
+  "success": true,
+  "total_questions": 1
+}
+```
+
+#### DELETE '/questions/<int:id>'
+- Deletes a question by id using url parameters
+- Returns id of deleted questions if successful
+- Sample: `curl -X DELETE http://127.0.0.1:5000/questions/2`
+```
+{
+  "deleted": 2,
+  "success": true,
+  "total_questions": 20
+}
+```
+
+
+
+#### POST '/quizzes'
+- Allows user to play the trivia game
+- Uses JSON request parameters of a chosen category and previous questions
+- Returns JSON object with random available questions which are not among previous used questions
+- Sample: `curl http://127.0.0.1:5000/quizzes -X POST -H "Content-Type: application/json" -d '{"previous_questions": [10, 11], "quiz_category": {"type": "Sports", "id": "6"}}'`
+```
+{
+  "question": {
+    "answer": "Bayern",
+    "category": 6,
+    "difficulty": 3,
+    "id": 25,
+    "question": "which is the champion team of the champions 2020?"
+  },
+  "success": true
+}
+```
--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,292 +1,171 @@
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+import unittest
+import json
 import random
+from flask_sqlalchemy import SQLAlchemy
 
-from models import setup_db, Question, Category
+from flaskr import create_app
+from models import setup_db
 
-QUESTIONS_PER_PAGE = 10
 
-db = SQLAlchemy()
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
 
-def paginate_questions(request, selection):
-    page = request.args.get("page", 1, type=int)
-    start = (page - 1) * QUESTIONS_PER_PAGE
-    end = start + QUESTIONS_PER_PAGE
-    questions = [question.format() for question in selection]
-    current_questions = questions[start:end]
-    return current_questions
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-    setup_db(app)
-    '''
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    '''
-    CORS(app, resources={'/': {'origins': '*'}})
-    '''
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    '''
+
+        self.new_question = {
+            'question': 'new question',
+            'answer': 'new answer',
+            'difficulty': 1,
+            'category': 1
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
     
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
-        return response
-    '''
-    @TODO: 
-    Create an endpoint to handle GET requests 
-    for all available categories.
-    '''
-    # GET categories route
-    @app.route("/categories", methods=["GET"])
-    def get_all_categories():
-        if request.method == "GET":
-            categories = Category.query.order_by(Category.id).all()
-            my_cat = {}
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
 
-            for cat in categories:
-                my_cat[cat.id] = cat.type
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
 
-            if len(my_cat) == 0:
-                abort(404)
+    # test get categories
+    def test_get_all_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
 
-            return jsonify({
-                'categories': my_cat,
-                'success': True
-            })
-    '''
-    @TODO: 
-    Create an endpoint to handle GET requests for questions, 
-    including pagination (every 10 questions). 
-    This endpoint should return a list of questions, 
-    number of total questions, current category, categories. 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+    # test get questions
 
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions. 
-    '''
-    # GET Question use pagination
+    def test_get_questions(self):
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
 
-    @app.route('/questions', methods=['GET'])
-    def get_questions():
-        if request.method == "GET":
-            questions = Question.query.all()
-            paginated_questions = paginate_questions(request, questions)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
 
-            if len(paginated_questions) == 0:
-                abort(404)
-            
-            categories = Category.query.all()
-            my_cat = {}
 
-            for cat in categories:
-                my_cat[cat.id] = cat.type
+    # test get questions with a wrong page parameter
 
-            return jsonify({
-                'success': True,
-                'total_questions': len(questions),
-                'categories': my_cat,
-                'questions': paginated_questions
-            })
+    def test_get_questions_404(self):
+        response = self.client().get('/questions?page=2000')
+        data = json.loads(response.data)
 
-    '''
-    @TODO: 
-    Create an endpoint to DELETE question using a question ID. 
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
 
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page. 
-    '''
-    # DELETE question
-    @app.route('/questions/<int:id>', methods=['DELETE'])
-    def delete_question(id):
-        if request.method == "DELETE":            
-            q = Question.query.filter_by(id=id).one_or_none()
-            if q is None:
-                abort(404)
-
-            q.delete()
-            return jsonify({
-                'deleted': id,
-                'success': True,
-                'total_questions': len(Question.query.all())
-            })
-    '''
-    @TODO: 
-    Create an endpoint to POST a new question, 
-    which will require the question and answer text, 
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab, 
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.  
-    '''
-
-    '''
-    @TODO: 
-    Create a POST endpoint to get questions based on a search term. 
-    It should return any questions for whom the search term 
-    is a substring of the question. 
-
-    TEST: Search by any phrase. The questions list will update to include 
-    only question that include that string within their question. 
-    Try using the word "title" to start. 
-    '''
-
-    # Create question POST METHOD
-    @app.route('/questions', methods=['POST'])
-    def create_a_question():
-        if request.method == "POST":
-            body = request.get_json()
-            question = body.get('question', None)
-            answer = body.get('answer', None)
-            difficulty = body.get('difficulty', None)
-            category = body.get('category',  None)
-            
-            searchTerm = body.get('searchTerm', None)
-            try:
-                if searchTerm:
-                    questions = Question.query.filter(Question.question.ilike(f"%{searchTerm}%")).all()
-                    paginated_questions = paginate_questions(request, questions)
-                    
-                    return jsonify({
-                        'success': True,
-                        'questions': paginated_questions,
-                        'total_questions': len(paginated_questions)
-                    })
-
-                else:
-                    q = Question(question=question, answer=answer, difficulty=difficulty, category=category)
-                    q.insert()
-
-                    questions = Question.query.order_by(Question.id).all()
-                    paginated_questions = paginate_questions(request, questions)
-
-                    return jsonify({
-                        'success': True,
-                        'question_created': q.question,
-                        'created': q.id,
-                        'questions': paginated_questions,
-                        'total_questions': len(Question.query.all())
-                    })
-            except:
-                abort(422)
-    
-    
-    '''
-    @TODO: 
-    Create a GET endpoint to get questions based on category. 
-
-    TEST: In the "List" tab / main screen, clicking on one of the 
-    categories in the left column will cause only questions of that 
-    category to be shown. 
-    '''
-    # GET questions by a category
-    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
-    def get_questions_by_category(category_id):
-        if request.method == "GET":
-            category = Category.query.filter_by(id=category_id).one_or_none()
-            if category is None:
-                abort(404)
-            try:
-                questions = Question.query.filter_by(category=category.id).all()
-                paginated_questions = paginate_questions(request, questions)
-
-                return jsonify({
-                    'success': True,
-                    'total_questions': len(Question.query.all()),
-                    'current_category': category.type,
-                    'questions': paginated_questions
-                })
-
-            except:
-                abort(400)
+    # test delete a question
+    def test_delete_question(self):
+        all_ids = []
+        q_all = json.loads(self.client().get('/questions').data)['questions']
         
+        for q in q_all:
+            all_ids.append(q['id'])
+            
+        deleted_id = random.choice(all_ids)
+        
+        response = self.client().delete(f'/questions/{deleted_id}')
+        data = json.loads(response.data)
 
-    '''
-    @TODO: 
-    Create a POST endpoint to get questions to play the quiz. 
-    This endpoint should take category and previous question parameters 
-    and return a random questions within the given category, 
-    if provided, and that is not one of the previous questions. 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], deleted_id)
 
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not. 
-    '''
-
-    @app.route('/play', methods=['POST'])
-    def play_game():
-        if request.method == "POST":
-            try:
-                body = request.get_json()
-                prev_questions = body.get('previous_questions', None)
-                category = body.get('quiz_category', None)
-
-                cat_id = category['id']
-                next_question = None
-                
-                if cat_id != 0:
-                    av_questions = Question.query.filter_by(category=cat_id).filter(Question.id.notin_((prev_questions))).all()    
-                else:
-                    av_questions = Question.query.filter(Question.id.notin_((prev_questions))).all()
-                
-                if len(av_questions) > 0:
-                    next_question = random.choice(av_questions).format()
-                
-                return jsonify({
-                    'question': next_question,
-                    'success': True,
-                })
-            except:
-                abort(422)
-
-
-    '''
-    @TODO: 
-    Create error handlers for all expected errors 
-    including 404 and 422. 
-    '''
-    @app.errorhandler(400)
-    def bad_request_error(error):
-        return jsonify({
-            "success": False,
-            "error": 400,
-            "message": "bad request"
-        }), 400
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({
-            "success": False,
-            "error": 404,
-            "message": "resource not found"
-        }), 404
     
-    @app.errorhandler(405)
-    def method_not_allowed(error):
-        return jsonify({
-            'success': False,
-            'error': 405,
-            'message': 'method not allowed'
-        }), 405
+    # test detele a questions with a id that is not in the database
+    def test_delete_question_404(self):
+        response = self.client().delete('/questions/5000')
+        data = json.loads(response.data)
 
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return jsonify({
-            "success": False,
-            "error": 422,
-            "message": "unprocessable"
-        }), 422
+        self.assertEqual(response.status_code, 404)
 
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return jsonify({
-            "success": False,
-            "error": 500,
-            "message": "internal server error"
-        }), 422
 
-    return app
+    # test create question
+
+    def test_create_question(self):
+        response = self.client().post('/questions', json=self.new_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_405_question_creation_not_allowed(self):
+        response = self.client().post('/questions/45', json=self.new_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
+
+
+    # test search with results
+
+    def test_search(self):
+        response = self.client().post('/questions', json={'searchTerm': 'invented'})
+
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_search_without_results(self):
+        response = self.client().post('/questions', json={'searchTerm':'asdf'})
+
+        data = json.loads(response.data)
+
+        self.assertEqual(data['total_questions'],0)
+        self.assertEqual(data['success'], True)
+    
+
+    # test get questions by category
+    def test_get_questions_by_category(self):
+        response = self.client().get('/categories/1/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['current_category'], 'Science')
+        self.assertEqual(data['success'], True)
+
+    def test_get_404_questions_by_category(self):
+        response = self.client().get('/categories/1000/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(data['success'], False)
+
+    # test quiz
+    def test_quiz(self):
+        quiz_round = {'previous_questions': [], 'quiz_category': {'type': 'Geography', 'id': 14}}
+        response = self.client().post('/play', json=quiz_round)
+        data = json.loads(response.data)
+
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_422_quiz(self):
+        response = self.client().post('/play', json={})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
